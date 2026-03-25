66b728f24e2ffe32c85f0f1ed07edc2800ccd9eb
Arda <arda.yusun184@escapestudios.ac.uk>
2026-03-25 17:33:05 +0000

    Created the functions needed for the dungeon generation

---
+++ Source/ProceduralDungeonGen/ADungeonGenerator.cpp
@@ -0,0 +1,93 @@
+﻿
+#include "ADungeonGenerator.h"
+
+
+AADungeonGenerator::AADungeonGenerator()
+{
+}
+
+
+void AADungeonGenerator::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	GenerateDungeon();
+}
+
+
+// Create the grid for the dungeon
+void AADungeonGenerator::InitializeGridLayout()
+{
+	Grid.SetNum( Width * Height );
+}
+
+
+// Gets a cell from the grid
+FRoomCell& AADungeonGenerator::GetCell( int32 X, int32 Y )
+{
+	return Grid[ X + Y * Width ];
+}
+
+
+// Check if the room cell is inside the grids range
+bool AADungeonGenerator::IsInsideGrid( int32 X, int32 Y ) const
+{
+	if ( (X >= 0 && X < Width) && (Y >= 0 && Y < Height) )
+	{
+		return true;
+	}
+	
+	return false;
+}
+
+
+// Gets a random point in the grid
+FIntPoint AADungeonGenerator::GetRandomPoint()
+{
+	int32 XPosition{ RandomStream.RandRange(0, Width - 1) };
+	int32 YPosition{ RandomStream.RandRange(0, Height - 1) };
+	FIntPoint Point{ XPosition, YPosition };
+	
+	return Point;
+}
+
+
+// Generates the START and END points in the grid
+void AADungeonGenerator::GenerateStartEndPoints( int32 Seed )
+{
+	StartPoint = GetRandomPoint();
+	EndPoint = GetRandomPoint();
+	
+	while ( EndPoint == StartPoint )
+	{
+		EndPoint = GetRandomPoint();
+	}
+	
+	GetCell( StartPoint.X, StartPoint.Y ).bIsStart = true;
+	GetCell( StartPoint.X, StartPoint.Y ).bIsRoom = true;
+	GetCell( EndPoint.X, EndPoint.Y ).bIsEnd = true;
+	GetCell( EndPoint.X, EndPoint.Y ).bIsRoom = true;
+}
+
+
+// Create a path straight from the START to the END
+void AADungeonGenerator::GenerateCriticalPath()
+{
+	FIntPoint GridLocation( Width, Height );
+	
+	
+	
+}
+
+
+// Generates the Dungeon
+void AADungeonGenerator::GenerateDungeon()
+{
+	int32 RandomSeed{ FMath::RandRange(0, 100) };
+	RandomStream.Initialize(RandomSeed);
+	
+	InitializeGridLayout();
+	GenerateStartEndPoints( RandomSeed );
+	GenerateCriticalPath();
+	
+}
---
+++ Source/ProceduralDungeonGen/ADungeonGenerator.h
@@ -0,0 +1,58 @@
+﻿
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ADungeonGenerator.generated.h"
+
+
+USTRUCT()
+struct FRoomCell
+{
+	GENERATED_BODY()
+	
+	bool bIsRoom = false;
+	bool bIsStart = false;
+	bool bIsEnd = false;
+	bool bIsVisited = false;
+};
+
+
+UCLASS()
+class PROCEDURALDUNGEONGEN_API AADungeonGenerator : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AADungeonGenerator();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	int32 Seed{ 0 };
+	
+	UPROPERTY()
+	TArray<FRoomCell> Grid;
+	UPROPERTY(EditAnywhere, Category = "Grid Size")
+	int32 Width{ 10 };
+	UPROPERTY(EditAnywhere, Category = "Grid Size")
+	int32 Height{ 10 };
+	
+	
+	UFUNCTION(BlueprintCallable)
+	void GenerateDungeon();
+	
+private:
+	void InitializeGridLayout();
+	bool IsInsideGrid( int32 X, int32 Y ) const;
+	void GenerateCriticalPath();
+	void GenerateStartEndPoints( int32 Seed );
+	FIntPoint GetRandomPoint();
+	
+	FRoomCell& GetCell( int32 X, int32 Y );
+	
+	FIntPoint StartPoint;
+	FIntPoint EndPoint;
+	FRandomStream RandomStream;
+};
